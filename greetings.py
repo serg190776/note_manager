c8c14c68ea9fd9a988d912f55bd0e19179fa7552
serg190776 <sergejkudravcev47818@gmail.com>
2024-12-21 18:08:07 +0300

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -1,9 +1,17 @@
-name=input('Здравствуите,как вас зовут?')
-print(name)
-age=int(input('Сколько вам полных лет?'))
-print(age)
-print('Через 5 лет вам будет')
-print(age+5)
+username=input('Введите имя пользователя:')
+print(username)
+
+title=input('Введите заголовок заметки:')
+print(title)
+content=input('Введите описание заметки:')
+print(content)
+status=input('Введите статус заметки:')
+print(status)
+created_date=(input(('Дата создания заметки в формате:("день-месяц-год"):')))
+print(created_date)
+issue_date=(input(('Дата истечения заметки в формате:("день-месяц-год"):')))
+print(issue_date)
+
 
 
 
