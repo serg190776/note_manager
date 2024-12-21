cac3bc21e061ed10df8c2c844f2bc97b662fcf95
serg190776 <sergejkudravcev47818@gmail.com>
2024-12-21 23:46:54 +0300

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -1,18 +1,15 @@
 username=input('Введите имя пользователя:')
 print(username)
-
 title=input('Введите заголовок заметки:')
 print(title)
 content=input('Введите описание заметки:')
 print(content)
 status=input('Введите статус заметки:')
 print(status)
-created_date=(input(('Дата создания заметки в формате:("день-месяц-год"):')))
+created_date=input('Введите дату создания заметки в формате "день-месяц-год":')
 print(created_date)
-issue_date=(input(('Дата истечения заметки в формате:("день-месяц-год"):')))
+issue_date=input('Введите дату истечения заметки в формате "день-месяц-год":')
 print(issue_date)
-
-
 
 
 
