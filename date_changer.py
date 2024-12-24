e8027de40cad19ffb2f9c4634a02e2ede920a4f4
serg190776 <sergejkudravcev47818@gmail.com>
2024-12-24 19:59:16 +0300

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,6 +1,21 @@
-name=input('Здравствуите,как вас зовут?')
-print(name)
-age=int(input('Сколько вам полных лет?'))
-print(age)
-print('Через 5 лет вам будет')
-print(age+5)
+username='Введите имя пользователя:Сергей'
+print(username)
+title='Введите заголовок заметки:SkillPlace'
+print(title)
+content='Введите описание заметки:Grade1.Этап1:Задание1'
+print(content)
+status='Введите статус заметки:Задание1'
+print(status)
+created_date='Введите дату создания заметки в формате "день-месяц-год":24-12-2024'
+print(created_date)
+temp_creadet_date='Введите дату создания заметки в формате "день-месяц":24-12'
+print(temp_creadet_date)
+issue_data='Введите дату истечения заметки в формате "день-месяц-год":15-01-2025'
+print(issue_data)
+temp_issue_data='Введите дату истечения заметки в формате "день-месяц":15-01'
+print(temp_issue_data)
+
+
+
+
+
